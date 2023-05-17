b424f04466573f3c9adc045133c5699e59fc16cd
ChanoMV <117382340+ChanoMV@users.noreply.github.com>
2023-05-17 11:11:07 -0300

    .

--- mr54289/src/main/java/com/portfolio/mr54289/Controler/CEducacion.java
+++ mr54289/src/main/java/com/portfolio/mr54289/Controler/CEducacion.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,7 +22,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("edu")
+@RequestMapping("/edu")
+@CrossOrigin (origins = "https://proyectoargprogr.web.app/")
 public class CEducacion {
     @Autowired
     SEducacion sEducacion;
--- mr54289/src/main/java/com/portfolio/mr54289/Controler/PersonaController.java
+++ mr54289/src/main/java/com/portfolio/mr54289/Controler/PersonaController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,6 +25,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/personas")
+@CrossOrigin (origins = "https://proyectoargprogr.web.app/")
 
 public class PersonaController {
     @Autowired
